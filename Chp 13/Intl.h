825e899ffa1847ebf473713f3da9c9e96b1afd40
ththfql <145094239+ththfql@users.noreply.github.com>
2026-06-17 14:52:19 +0900

    Final Exam

---
+++ Chp 13/Intl.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+// Intl.h
+
+#ifndef GUARD_Intl_h
+#define GUARD_Intl_h
+
+#include <iostream>
+
+#include "Core.h"
+
+using namespace std;
+
+class Intl : public Core {
+public:
+	Intl() : toiec(0) {};
+	Intl(istream& is) { read(is); };
+	double grade() const;
+	istream& read(istream&);
+
+private:
+	double toiec; // 졸업 논문
+	Intl* clone() const { return new Intl(*this); }
+};
+
+#endif
